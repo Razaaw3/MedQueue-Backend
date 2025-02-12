d6e7115ef8a179be3ad419620fb0cf4e9f79921e
Awais Raza <razaaw3@gmail.com>
2025-02-12 23:03:56 +0500

    auth controller modified

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,12 @@
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import User from "../models/user.model.js";
-import { validateEmail } from "../utils/validators.js";
-import ApiError from "../utils/errors/ApiError.js";
-import { ApiResponse } from "../utils/errors/ApiResponse.js";
-import { asyncHandler } from "../utils/errors/asyncHandler.js";
-import { uploadOnCloudinary } from "../utils/cloudinaryImageHandling.js";
-import sendEmail from "../utils/sendEmail.js";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import User from '../models/user.model.js';
+import {validateEmail} from '../utils/validators.js';
+import ApiError from '../utils/errors/ApiError.js';
+import {ApiResponse} from '../utils/errors/ApiResponse.js';
+import {asyncHandler} from '../utils/errors/asyncHandler.js';
+import {uploadOnCloudinary} from '../utils/cloudinaryImageHandling.js';
+import sendEmail from '../utils/sendEmail.js';
 
 const generateOTP = () =>
   Math.floor(100000 + Math.random() * 900000).toString();
@@ -17,39 +17,39 @@ const hashOTP = (otp) => bcrypt.hashSync(otp, 10);
 // Generate JWT Tokens
 const generateToken = (user) => {
   const accessToken = jwt.sign(
-    { _id: user._id, role: user.role },
+    {_id: user._id, role: user.role},
     process.env.JWT_SECRET,
-    { expiresIn: "15m" }
+    {expiresIn: '15m'}
   );
 
   const refreshToken = jwt.sign(
-    { _id: user._id },
+    {_id: user._id},
     process.env.JWT_REFRESH_SECRET,
-    { expiresIn: "7d" }
+    {expiresIn: '7d'}
   );
 
-  return { accessToken, refreshToken };
+  return {accessToken, refreshToken};
 };
 
 // Register User
 const register = asyncHandler(async (req, res) => {
-  const { name, email, password, role = "registeredUser" } = req.body;
+  const {name, email, password, role = 'registeredUser'} = req.body;
 
   if (!name || !email || !password) {
-    throw new ApiError(400, "All fields are required");
+    throw new ApiError(400, 'All fields are required');
   }
 
   if (!validateEmail(email)) {
-    throw new ApiError(400, "Invalid email format");
+    throw new ApiError(400, 'Invalid email format');
   }
 
-  const existingUser = await User.findOne({ email });
+  const existingUser = await User.findOne({email});
 
   if (existingUser) {
     if (existingUser.isVerified) {
       throw new ApiError(
         409,
-        "User with this email already exists and is verified."
+        'User with this email already exists and is verified.'
       );
     }
 
@@ -61,18 +61,18 @@ const register = asyncHandler(async (req, res) => {
 
     await sendEmail(
       email,
-      "Email Verification - MedQueue",
-      "emailVerification.html",
-      { otp }
+      'Email Verification - MedQueue',
+      'emailVerification.html',
+      {otp}
     );
 
     return res
       .status(200)
-      .json(new ApiResponse(200, { email }, "OTP resent for verification."));
+      .json(new ApiResponse(200, {email}, 'OTP resent for verification.'));
   }
 
   let profileUrl =
-    "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png";
+    'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png';
   const profileLocalPath = req.files?.profile?.[0]?.path;
 
   if (profileLocalPath) {
@@ -100,43 +100,41 @@ const register = asyncHandler(async (req, res) => {
 
   await sendEmail(
     email,
-    "Email Verification - MedQueue",
-    "emailVerification.html",
-    { otp }
+    'Email Verification - MedQueue',
+    'emailVerification.html',
+    {otp}
   );
 
   return res
     .status(201)
-    .json(
-      new ApiResponse(201, { email }, "OTP sent to email for verification.")
-    );
+    .json(new ApiResponse(201, {email}, 'OTP sent to email for verification.'));
 });
 
 // Verify OTP
 const verifyOTP = asyncHandler(async (req, res) => {
-  const { email, otp } = req.body;
+  const {email, otp} = req.body;
 
   if (!email || !otp) {
-    throw new ApiError(400, "Email and OTP are required");
+    throw new ApiError(400, 'Email and OTP are required');
   }
 
-  const user = await User.findOne({ email });
+  const user = await User.findOne({email});
 
   if (!user) {
-    throw new ApiError(404, "User not found");
+    throw new ApiError(404, 'User not found');
   }
 
   if (user.isVerified) {
-    throw new ApiError(400, "User is already verified");
+    throw new ApiError(400, 'User is already verified');
   }
 
   if (!user.otp || user.otpExpiry < new Date()) {
-    throw new ApiError(400, "OTP expired. Please request a new OTP.");
+    throw new ApiError(400, 'OTP expired. Please request a new OTP.');
   }
 
   const isOTPValid = bcrypt.compareSync(otp, user.otp);
   if (!isOTPValid) {
-    throw new ApiError(400, "Invalid OTP");
+    throw new ApiError(400, 'Invalid OTP');
   }
 
   user.isVerified = true;
@@ -146,94 +144,95 @@ const verifyOTP = asyncHandler(async (req, res) => {
 
   return res
     .status(200)
-    .json(new ApiResponse(200, { email }, "Email verified successfully"));
+    .json(new ApiResponse(200, {email}, 'Email verified successfully'));
 });
 
 // Resend OTP
 const resendOTP = asyncHandler(async (req, res) => {
-  const { email } = req.body;
+  const {email} = req.body;
 
   if (!email) {
-    throw new ApiError(400, "Email is required");
+    throw new ApiError(400, 'Email is required');
   }
 
-  const user = await User.findOne({ email });
+  const user = await User.findOne({email});
 
   if (!user) {
-    throw new ApiError(404, "User not found");
+    throw new ApiError(404, 'User not found');
   }
 
   if (user.isVerified) {
-    throw new ApiError(400, "User is already verified");
+    throw new ApiError(400, 'User is already verified');
   }
 
   // if (user.otpExpiry && user.otpExpiry > new Date()) {
   //   throw new ApiError(429, "Please wait before requesting a new OTP.");
   // }
+  console.log('first');
 
   const newOtp = generateOTP();
   user.otp = hashOTP(newOtp);
   user.otpExpiry = new Date(Date.now() + 10 * 60 * 1000);
   await user.save();
 
-  await sendEmail(email, "New OTP - MedQueue", "newOtp.html", { newOtp });
+  await sendEmail(email, 'New OTP - MedQueue', 'newOtp.html', {newOtp});
 
   return res
     .status(200)
-    .json(new ApiResponse(200, { email }, "New OTP sent to email."));
+    .json(new ApiResponse(200, {email}, 'New OTP sent to email.'));
 });
 
 // Login
 const login = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+  const {email, password} = req.body;
 
   if (!email || !password) {
-    throw new ApiError(400, "Email and password are required");
+    throw new ApiError(400, 'Email and password are required');
   }
 
   if (!validateEmail(email)) {
-    throw new ApiError(400, "Invalid email format");
+    throw new ApiError(400, 'Invalid email format');
   }
 
-  const user = await User.findOne({ email });
+  const user = await User.findOne({email});
 
   if (!user) {
-    throw new ApiError(401, "Invalid credentials");
+    throw new ApiError(401, 'Invalid credentials');
   }
 
   if (!user.isVerified) {
-    throw new ApiError(403, "Email not verified. Please verify your account.");
+    throw new ApiError(403, 'Email not verified. Please verify your account.');
   }
 
   if (user.failedLoginAttempts >= 5) {
-    throw new ApiError(429, "Too many failed login attempts. Try again later.");
+    throw new ApiError(429, 'Too many failed login attempts. Try again later.');
   }
 
   const isPasswordValid = await bcrypt.compare(password, user.password);
   if (!isPasswordValid) {
     user.failedLoginAttempts += 1;
     await user.save();
-    throw new ApiError(401, "Invalid credentials");
+    throw new ApiError(401, 'Invalid credentials');
   }
 
   user.failedLoginAttempts = 0;
   await user.save();
 
-  const { accessToken, refreshToken } = generateToken(user);
+  const {accessToken, refreshToken} = generateToken(user);
 
   return res
     .status(200)
-    .cookie("access_token", accessToken, {
+    .cookie('access_token', accessToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.NODE_ENV === 'production',
     })
     .json(
       new ApiResponse(
         200,
-        { accessToken, refreshToken, user },
-        "User logged in successfully"
+        {accessToken, refreshToken, user},
+        'User logged in successfully'
       )
     );
 });
 
-export { register, verifyOTP, resendOTP, login };
+export {register, verifyOTP, resendOTP, login};
