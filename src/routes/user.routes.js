c514dcffd39f59f4de1190064ded2724ecbd562c
Irfan Arif <irfanarif444@gmail.com>
2025-04-19 17:31:28 +0500

    basic structure for PrivacySettings model/controller added.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,49 +1,30 @@
-import express from 'express';
+import express from "express";
 import {
   verifyToken,
   isRegisteredUser,
-  isGuest,
-} from '../middleware/auth.middleware.js';
+} from "../middleware/auth.middleware.js";
 import {
   getUserProfile,
   updateUserProfile,
-  getUserTokenHistory,
-  upgradeToRegisteredUser,
-  getUserToken,
-  getUserAppointmentsByStatus,
   updateProfileImage,
-} from '../controllers/user.controller.js';
-import {upload} from '../middleware/multer.middleware.js';
+} from "../controllers/user.controller.js";
+import { upload } from "../middleware/multer.middleware.js";
 
 const router = express.Router();
 
-router.get('/profile', verifyToken, isRegisteredUser, getUserProfile);
-router.put('/update-profile', verifyToken, isRegisteredUser, updateUserProfile);
+router.get("/profile", verifyToken, isRegisteredUser, getUserProfile);
+router.put("/update-profile", verifyToken, isRegisteredUser, updateUserProfile);
 router.patch(
-  '/update-image',
+  "/update-image",
   verifyToken,
   upload.fields([
     {
-      name: 'image',
+      name: "image",
       maxCount: 1,
     },
   ]),
   isRegisteredUser,
   updateProfileImage
-);
-router.get(
-  '/token-history',
-  verifyToken,
-  isRegisteredUser,
-  getUserTokenHistory
-);
-router.get('/user-token', verifyToken, isRegisteredUser, getUserToken);
-router.post('/upgrade', verifyToken, isGuest, upgradeToRegisteredUser);
-router.get(
-  '/appointment-history',
-  verifyToken,
-  isRegisteredUser,
-  getUserAppointmentsByStatus
 );
 
 export default router;
