13e0913995ba4cbf7e5382fcc146183d7cc5c302
Awais Raza <razaaw3@gmail.com>
2025-05-25 10:18:18 +0500

    Socket testing

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ export const io = new Server(server, {
   cors: {
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['*'],
   },
 });
 
@@ -36,7 +38,7 @@ app.use(
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['*'],
   })
 );
 
