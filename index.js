aed8f56d5069e98298229e45dda5c086b5626e30
Irfan Arif <irfanarif444@gmail.com>
2025-05-28 00:45:54 +0500

    fixed api calling issue using allowedHeaders

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import connectDB from './src/config/db.config.js';
-import authRoutes from './src/routes/auth.routes.js';
-import adminRoutes from './src/routes/admin.routes.js';
-import userRoutes from './src/routes/user.routes.js';
-import tokenRoutes from './src/routes/tokens.routes.js';
-import clinicRoutes from './src/routes/clinic.routes.js';
-import privacyRoutes from './src/routes/privacy.routes.js';
-import dashboardRoutes from './src/routes/dashboard.routes.js';
-import {Server} from 'socket.io';
-import http from 'http';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import connectDB from "./src/config/db.config.js";
+import authRoutes from "./src/routes/auth.routes.js";
+import adminRoutes from "./src/routes/admin.routes.js";
+import userRoutes from "./src/routes/user.routes.js";
+import tokenRoutes from "./src/routes/tokens.routes.js";
+import clinicRoutes from "./src/routes/clinic.routes.js";
+import privacyRoutes from "./src/routes/privacy.routes.js";
+import dashboardRoutes from "./src/routes/dashboard.routes.js";
+import { Server } from "socket.io";
+import http from "http";
+import cors from "cors";
 
 dotenv.config();
 
@@ -19,14 +19,8 @@ const app = express();
 const server = http.createServer(app);
 
 export const io = new Server(server, {
-  cors: {
-    origin: '*',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['*'],
-  },
-  transports: ['websocket', 'polling'],
-  path: '/socket.io/',
+  transports: ["websocket", "polling"],
+  path: "/socket.io/",
   pingTimeout: 60000,
   pingInterval: 25000,
 });
@@ -38,10 +32,10 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: '*',
+    origin: ["*", "https://localhost:3000", "http://localhost:3000"],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['*'],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["*", "Authorization", "Content-Type"],
   })
 );
 
@@ -50,13 +44,13 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/clinic', clinicRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/admin', adminRoutes);
-app.use('/api/user', userRoutes);
-app.use('/api/token', tokenRoutes);
-app.use('/api/privacy', privacyRoutes);
-app.use('/api/dashboard', dashboardRoutes);
+app.use("/api/clinic", clinicRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/token", tokenRoutes);
+app.use("/api/privacy", privacyRoutes);
+app.use("/api/dashboard", dashboardRoutes);
 
 const PORT = process.env.PORT || 8000;
 
