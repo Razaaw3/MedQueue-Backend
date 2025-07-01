127dcac11fe6ecca6832c0a79cd9066724bc43ee
Irfan Arif <irfanarif444@gmail.com>
2025-07-01 17:17:07 +0500

    added deployed frontend URL

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(
       "*",
       "http://localhost:3001",
       "http://localhost:3000",
-      "http://https://dashboard.medqueue.live/",
+      "https://dashboard.medqueue.live/",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
