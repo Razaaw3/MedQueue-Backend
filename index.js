caf2ee35f5dc09efe467285bb3c32817caeafd93
Awais Raza <razaaw3@gmail.com>
2025-07-14 22:46:47 +0500

    cors removed

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import connectDB from "./src/config/db.config.js";
-import authRoutes from "./src/routes/auth.routes.js";
-import adminRoutes from "./src/routes/admin.routes.js";
-import userRoutes from "./src/routes/user.routes.js";
-import tokenRoutes from "./src/routes/tokens.routes.js";
-import clinicRoutes from "./src/routes/clinic.routes.js";
-import privacyRoutes from "./src/routes/privacy.routes.js";
-import dashboardRoutes from "./src/routes/dashboard.routes.js";
-import { Server } from "socket.io";
-import http from "http";
-import cors from "cors";
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import connectDB from './src/config/db.config.js';
+import authRoutes from './src/routes/auth.routes.js';
+import adminRoutes from './src/routes/admin.routes.js';
+import userRoutes from './src/routes/user.routes.js';
+import tokenRoutes from './src/routes/tokens.routes.js';
+import clinicRoutes from './src/routes/clinic.routes.js';
+import privacyRoutes from './src/routes/privacy.routes.js';
+import dashboardRoutes from './src/routes/dashboard.routes.js';
+import {Server} from 'socket.io';
+import http from 'http';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -19,14 +19,14 @@ const app = express();
 const server = http.createServer(app);
 
 export const io = new Server(server, {
-  cors: {
-    origin: "*",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["*"],
-  },
-  transports: ["websocket", "polling"],
-  path: "/socket.io/",
+  // cors: {
+  //   origin: "*",
+  //   credentials: true,
+  //   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  //   allowedHeaders: ["*"],
+  // },
+  transports: ['websocket', 'polling'],
+  path: '/socket.io/',
   pingTimeout: 60000,
   pingInterval: 25000,
 });
@@ -36,32 +36,32 @@ connectDB(io);
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3001",
-      "http://localhost:3000",
-      "https://www.dashboard.medqueue.live",
-      "https://dashboard.medqueue.live",
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Authorization", "Content-Type"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:3001",
+//       "http://localhost:3000",
+//       "https://www.dashboard.medqueue.live",
+//       "https://dashboard.medqueue.live",
+//     ],
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+//     allowedHeaders: ["Authorization", "Content-Type"],
+//   })
+// );
 
 app.use((req, res, next) => {
   req.io = io;
   next();
 });
 
-app.use("/api/clinic", clinicRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/token", tokenRoutes);
-app.use("/api/privacy", privacyRoutes);
-app.use("/api/dashboard", dashboardRoutes);
+app.use('/api/clinic', clinicRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/token', tokenRoutes);
+app.use('/api/privacy', privacyRoutes);
+app.use('/api/dashboard', dashboardRoutes);
 
 const PORT = process.env.PORT || 8000;
 
