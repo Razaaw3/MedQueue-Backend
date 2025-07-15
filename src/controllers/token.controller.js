ef5f5e006a1aac24695b302d807b0a9ff330a914
Awais Raza <razaaw3@gmail.com>
2025-07-15 07:31:27 +0500

    Testing

--- src/controllers/token.controller.js
+++ src/controllers/token.controller.js
@@ -143,11 +143,9 @@ export const generateToken = asyncHandler(async (req, res) => {
         estimatedTurnTime = openingTime;
         console.log('estimatedTurnTime (from openingTime):', estimatedTurnTime);
         const now = DateTime.fromJSDate(getCurrentAppTime())
-          // .plus({hours: 5})
+          .plus({hours: 5})
           .toJSDate();
         console.log('now:', now);
-        queue.waitTime = -300;
-        console.log('Now is : ', now);
         if (DateTime.fromJSDate(now) > DateTime.fromJSDate(openingTime)) {
           queue.waitTime = DateTime.fromJSDate(now).diff(
             DateTime.fromJSDate(openingTime),
@@ -163,6 +161,7 @@ export const generateToken = asyncHandler(async (req, res) => {
       // .plus({hours: 5})
       .toJSDate();
     console.log('now:', now);
+    queue.waitTime = -300;
     if (DateTime.fromJSDate(now) > DateTime.fromJSDate(openingTime)) {
       if (!queue || queue.upcomingTokenIds.length === 0) {
         queue.waitTime = DateTime.fromJSDate(now).diff(
