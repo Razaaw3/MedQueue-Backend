f442d5d76ffb243653595fbc3f9bee8ffe88d466
Awais Raza <razaaw3@gmail.com>
2025-07-15 07:13:21 +0500

    Testing

--- src/controllers/token.controller.js
+++ src/controllers/token.controller.js
@@ -151,6 +151,7 @@ export const generateToken = asyncHandler(async (req, res) => {
             DateTime.fromJSDate(openingTime),
             'minutes'
           ).minutes;
+          queue.waitTime = queue.waitTime - 300;
         }
       }
     }
